353807eb0d66ccd205166b528d69b80cab504466
Teeclever <omotoboraomotayo@gmail.com>
2023-08-30 22:06:18 +0100

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - An entry for printing out a string
+ * Return: 0
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - An entry to print out a string
+ * Return: 0 on success
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+}
