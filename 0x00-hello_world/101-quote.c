5e8fbf11bde9da9247aa5dc6c11fa8f933602334
Teeclever <omotoboraomotayo@gmail.com>
2023-10-27 22:47:58 +0100

    done implementing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main()
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+
+	return (1);
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(float));
+
+
+	return (0);
+
+}
